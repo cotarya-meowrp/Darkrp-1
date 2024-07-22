cb4397e06c0f5c4c8b8eaa6c4e61e9c93710bd6c
cat228 <131574761+oleggamer351@users.noreply.github.com>
2024-07-22 21:04:57 +0900

    added shotgun to no license weapons

--- gamemodes/darkrp/gamemode/config/licenseweapons.lua
+++ gamemodes/darkrp/gamemode/config/licenseweapons.lua
@@ -2,3 +2,4 @@ GM.NoLicense["weapon_physcannon"] = true
 GM.NoLicense["weapon_physgun"] = true
 GM.NoLicense["gmod_tool"] = true
 GM.NoLicense["gmod_camera"] = true
+GM.NoLicense["weapon_shotgun"] = true
